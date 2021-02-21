a6d607f3b526abc8bc46cfe0ad7e23fbb55f3382
lighty <hikalin8686@gmail.com>
2021-02-22 00:02:19 +0900

    add spec

--- spec/oas_divider_spec.rb
+++ spec/oas_divider_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe OasDivider do
     expect(OasDivider::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:oas_divider) { OasDivider::Cli.new('spec/fixtures/openapi.yaml') }
+  before { oas_divider.divide }
+  after { FileUtils.rm_r %w(swagger_root.yml components paths) } # TODO 出力ディレクトリ指定できるようにしないと危険
+
+  specify 'ファイルが作成されること', :aggrigate_failures do
+    expect(File).to exist('swagger_root.yml')
   end
 end
